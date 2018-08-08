fd620d8cfd75505b65ea3517603d0aa5d973cf03
Calvin Wong <calwong42991@gmail.com>
2018-08-08 13:54:31 -0700

    LengthofLongestSubstring completed

---
+++ LeetCode/lengthOfLongestSubstring.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let strSet = new Set();
+  let rmvIdx = 0;
+  let curIdx = 0;
+  let longestStr = 0;
+  while(curIdx < s.length) {
+    let l = s[curIdx];
+    if(!strSet.has(l)) {
+      strSet.add(l);
+      curIdx++;
+      longestStr = Math.max(strSet.size, longestStr); 
+    } else {
+      strSet.delete(s[rmvIdx]);
+      rmvIdx++;
+    }
+  }
+  return longestStr;
+};
+
+console.log(lengthOfLongestSubstring('calvinwongcalvin'))
